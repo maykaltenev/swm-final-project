c13ac3ac1c553287bbfa3edd40493b473b88695d
altinawiwaseem <wasemm@live.com>
2022-11-02 15:33:02 +0100

    editting the user model and controller

--- server/models/user.js
+++ server/models/user.js
@@ -3,11 +3,6 @@ import { generateRobohashAvatar } from "../helpers/avatar.js";
 const { Schema, model } = mongoose;
 
 const UserSchema = new Schema({
-  userName: {
-    type: String,
-    required: [true, "the user must have a useName"],
-    unique: true,
-  },
   email: {
     type: String,
     required: [true, "the user must have an Email"],
