4241e2154282fcb3bd8610aa6ece08625ec33d98
altinawiwaseem <wasemm@live.com>
2022-11-06 16:17:34 +0100

    questions Schema

---
+++ server/models/questions.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const QuestionsSchema = new Schema({
+  questionText: {
+    type: String,
+    required: [true, "question is required"],
+  },
+  options: {
+    type: [{ option: { type: String }, isCorrect: Boolean }],
+    required: [true, "question should have options"],
+  },
+  correctOptions: {
+    type: Number,
+    required: [true, "question should have correct options"],
+  },
+  mark: {
+    type: Number,
+    required: [true, "question should have mark"],
+  },
+  difficultyLevel: { type: String },
+  explanation: String,
+  image: String,
+  questionNumber: Number,
+});
+
+const question = model("Question", QuestionsSchema);
+
+export default question;
