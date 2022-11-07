eec033d1538c6024455b026dd02fac44de720953
sgujuluva <sanghee04@gmail.com>
2022-11-07 09:47:34 +0100

    app.js formatted

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,14 @@
 import Register from "./components/Register";
 import Login from "./components/Login";
 import { Route, Routes } from "react-router-dom";
-import Counter from "./components/Counter/Counter";
 
 function App() {
   return (
     <div className="App">
-      <header></header>
-      <Counter />
-      <main>
-        <Routes>
-          <Route path="/" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </main>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
