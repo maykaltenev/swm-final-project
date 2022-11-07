4a8d7531eefb6b8986926e84d2e746ff44c2ce30
sgujuluva <sanghee04@gmail.com>
2022-11-07 09:42:55 +0100

    counter folder deleted

--- client/src/components/Counter/counter.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Counter from "./Counter";
-
-test("<Counter/>", () => {
-  render(<Counter />);
-
-  const plusButton = screen.getByTestId("plusbutton");
-});
