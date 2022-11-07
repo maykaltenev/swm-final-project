6a38035512c13600e483a9e43bada180f38154ed
altinawiwaseem <wasemm@live.com>
2022-11-07 09:50:02 +0100

    s

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 /* import "./App.css"; */
->>>>>>> f0720a400aa31ae51eba05d77ab024e05e680be4
 import Register from "./components/Register";
 import Login from "./components/Login";
 import { Route, Routes } from "react-router-dom";
