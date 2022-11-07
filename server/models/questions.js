2ad5004fc5ae1d436580ee1009b43a2d55696f01
altinawiwaseem <wasemm@live.com>
2022-11-07 09:56:12 +0100

    questions model update

--- server/models/questions.js
+++ server/models/questions.js
@@ -25,16 +25,7 @@ export const QuestionsSchema = new Schema({
   questionNumber: Number,
 });
 
-<<<<<<< HEAD
-const javaScript = model("JavaScript", QuestionsSchema);
-const react = model("React", QuestionsSchema);
-const nodeJs = model("NodeJs", QuestionsSchema);
-const mongoDB = model("MongoDB", QuestionsSchema);
-=======
 export const javaScript = model("JavaScript", QuestionsSchema);
 export const react = model("React", QuestionsSchema);
 export const nodeJs = model("NodeJs", QuestionsSchema);
 export const mongoDB = model("MongoDB", QuestionsSchema);
-
->>>>>>> d6ca0a6940aab5a79f34e9ee0c71f88fff50e092
-
