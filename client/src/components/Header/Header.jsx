2e327e3e69a996eb1bdfc8599408c7611752f376
sgujuluva <sanghee04@gmail.com>
2022-11-16 14:50:27 +0100

    able to access the google user

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from "react-router-dom";
+import { UserContext } from "../Context/UserContext";
 //image
 import Logo from "../../assets/logo.png"
 function Header() {
+    const { googleUser, setGoogleUser } = useContext(UserContext);
   return (
     <div>
         <img src={Logo} width = "70px" height="50px" alt="" />
+        <Link to="/register">
+        <button>Login</button>
+        </Link>
+        {/* if google user found */}
+        {googleUser && 
+        <div>
+            <img src = {googleUser.picture}alt="userpicture"/>
+            <h3>{googleUser.name}</h3>
+        </div>}
     </div>
   )
 }
