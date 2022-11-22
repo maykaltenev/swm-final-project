cdb8a1d752445c7087622930e85692c08996ec77
sgujuluva <sanghee04@gmail.com>
2022-11-22 12:05:14 +0100

    user routes written for google login

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,6 +5,7 @@ import {
   logout,
   updateQuizTimer,
   getUserData,
+  GoogleStrategy
 } from "../controllers/userControllers.js";
 const router = Router();
 
@@ -22,5 +23,36 @@ router.get("/logout", logout);
 
 //http:localhost:5000/user/addTimer
 router.patch("/addTimer", updateQuizTimer);
+
+/**
+ * GOOGLE LOGIN PATHS
+ */
+
+ router.get('/google', passport.authenticate('google', {scope: ['profile', 'email']}))
+
+
+ router.get('/google/callback', passport.authenticate('google', {
+     failureRedirect: '/users/google/failure',
+     session: false
+ }), async (req, res) => {
+ 
+     console.log('from google callback: id is', req.user._id)
+ 
+     // User is the class. req.user is a new User
+     const token = await req.user.generateToken('1d')
+ 
+     res.cookie('cookiename', token)
+ 
+     res.redirect('http://localhost:3000/glogin/' + req.user._id)
+ })
+ 
+ router.get('/glogin/:id', async (req, res) => {
+ 
+     console.log('from glogin: id is', req.params.id)
+ 
+     const user = await User.findById(req.params.id).select('-__v -pass')
+ 
+     res.send({success: true, user})
+ })
 
 export default router;
