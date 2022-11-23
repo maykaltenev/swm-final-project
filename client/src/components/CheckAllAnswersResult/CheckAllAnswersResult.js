2edf5cbd56f1c5d03f044b3ad9e7bc7cc2c67620
sgujuluva <sanghee04@gmail.com>
2022-11-23 17:13:54 +0100

    able to mark correct answers with green

--- client/src/components/CheckAllAnswersResult/CheckAllAnswersResult.js
+++ client/src/components/CheckAllAnswersResult/CheckAllAnswersResult.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import "./CheckAllAnswersResult.css"
 
 function CheckAllAnswersResult({allQues}) {
     console.log("all ques is", allQues)
   return (
     <div key={allQues._id}>
-  {allQues?.questions?.map(item => 
+  {allQues?.questions?.map((item,i )=> 
   <>
-  <h5>{item.questionText}</h5>
-  <div>{item?.options?.map(e =>
-    <li>{e.option}</li> )} 
+  <h5>{i + 1}) {item.questionText}</h5>
+  <div >{item?.options?.map(e =>
+    <li  className = {e.isCorrect === true ? "green" : ""}>{e.option}    
+      </li>
+   
+    )} 
     </div>
     <h5>Explanation:</h5>
     <h5>{item.explanation}</h5>
