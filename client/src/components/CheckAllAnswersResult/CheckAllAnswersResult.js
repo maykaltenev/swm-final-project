faef0d4ae854c5a83324fed7bdf287cdf871aaed
sgujuluva <sanghee04@gmail.com>
2022-11-23 16:21:02 +0100

    explanation and questions are showing

--- client/src/components/CheckAllAnswersResult/CheckAllAnswersResult.js
+++ client/src/components/CheckAllAnswersResult/CheckAllAnswersResult.js
@@ -3,12 +3,15 @@ import React from 'react'
 function CheckAllAnswersResult({allQues}) {
     console.log("all ques is", allQues)
   return (
-    <div>
+    <div key={allQues._id}>
   {allQues?.questions?.map(item => 
   <>
   <h5>{item.questionText}</h5>
   <div>{item?.options?.map(e =>
-    <li>{e.option}</li> )} </div>
+    <li>{e.option}</li> )} 
+    </div>
+    <h5>Explanation:</h5>
+    <h5>{item.explanation}</h5>
     </>
     
     )}
