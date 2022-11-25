27c5ecb621f6e5399e171fa173f05b3f22bac2eb
sgujuluva <sanghee04@gmail.com>
2022-11-25 13:55:12 +0100

    result comp done but expect to input type text

--- client/src/components/CheckAllAnswersResult/CheckAllAnswersResult.js
+++ client/src/components/CheckAllAnswersResult/CheckAllAnswersResult.js
@@ -4,31 +4,46 @@ import "./CheckAllAnswersResult.css";
 function CheckAllAnswersResult({ allQues }) {
   const wrongAnswersArr = [];
   allQues?.userWrongAnswer?.map((item) => wrongAnswersArr.push(item.option));
-console.log("all ques",allQues)
 
-//if user doesn't enter any ans
-const noAnswerArr = []
-noAnswerArr.push(allQues?.resultArray?.map(ques => ques.question._id))
-console.log(noAnswerArr[0])
+  //if user doesn't enter any ans
+  const noAnswerArr = [];
+  noAnswerArr.push(allQues?.resultArray?.map((ques) => ques.question._id));
+
+  //user correctarray
+  const correctArray = [];
+  const wrongArray = [];
+  
+  allQues?.resultArray?.map((item) =>
+    item.correct === true
+      ? correctArray.push(item.question._id)
+      : wrongArray.push(item.question._id)
+  );
 
   return (
     <div key={allQues._id}>
       {allQues?.allQuestion?.questions?.map((question, i) => (
         <div key={i}>
- 
-          <h5 className={allQues?.resultArray?.map(correct => correct.correct
- === true )? "gray" : "red"}>
+          <h5
+            className={
+              correctArray.includes(question._id)
+                ? "lightgreen"
+                : wrongArray.includes(question._id)
+                ? "red"
+                : "gray"
+            }
+          >
             {i + 1}. {question.questionText}
           </h5>
 
           <div key={question._id}>
             {question?.options?.map((option, i) => (
-              <li key={i}
+              <li
+                key={i}
                 className={
                   option.isCorrect === true
                     ? "green"
                     : wrongAnswersArr.includes(option.option)
-                    ? "red"                     
+                    ? "red"
                     : ""
                 }
               >
