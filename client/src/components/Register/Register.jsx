3869b109442aa94bee95a38c5491c66dd628e64e
sgujuluva <sanghee04@gmail.com>
2022-11-28 22:35:15 +0100

    register page css given

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -25,46 +25,68 @@ export default function Register() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <div>
-          <p>Register</p>
-          <input
-            label="First Name"
-            placeholder="First name"
-            type="text"
-            autoComplete="firstName"
-            name="firstName"
-            required
-          />
-          <input
-            label="Last Name"
-            placeholder="Last name"
-            type="text"
-            autoComplete="lastName"
-            name="lastName"
-            required
-          />
-          <input
-            label="Email"
-            placeholder=" E-mail"
-            type="email"
-            autoComplete="email"
-            name="email"
-            required
-          />
+        <div className="lg:w-2/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col m-3 w-full  m-10 font-poppins">
+          <p className="title-font text-2xl mb-4">Register</p>
+          <div class="relative mb-4">
+            <input
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              label="First Name"
+              placeholder="First name"
+              type="text"
+              autoComplete="firstName"
+              name="firstName"
+              required
+            />
+          </div>
+          <div class="relative mb-4">
+            <input
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              label="Last Name"
+              placeholder="Last name"
+              type="text"
+              autoComplete="lastName"
+              name="lastName"
+              required
+            />
+          </div>
+          <div class="relative mb-4">
+            <input
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              label="Email"
+              placeholder=" E-mail"
+              type="email"
+              autoComplete="email"
+              name="email"
+              required
+            />
+          </div>
+          <div class="relative mb-4">
+            <input
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              label="Password"
+              placeholder="Password"
+              type="password"
+              autoComplete="new-password"
+              name="password"
+              required
+            />
+          </div>
 
-          <input
-            label="Password"
-            placeholder="Password"
-            type="password"
-            autoComplete="new-password"
-            name="password"
-            required
-          />
-          <button type="submit">Register</button>
+          <button
+            className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mb-4"
+            type="submit"
+          >
+            Register
+          </button>
+
           <p>
             Already have an account ?{" "}
             {
-              <Link variant="contained" to="/login">
+              <Link
+                className="text-blue-600 hover:text-primary-bg"
+                variant="contained"
+                to="/login"
+              >
                 Login
               </Link>
             }
