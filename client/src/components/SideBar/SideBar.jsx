0891a26033c3df60e8918c033b08b4b9a8fb9382
Maykal Tenev <michaeltenev@gmail.com>
2022-11-30 16:10:09 +0100

    adding a hover on the sidebar

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -2,39 +2,64 @@ import React from "react";
 import { RocketIcon, LayersIcon, ArchiveIcon } from "@radix-ui/react-icons";
 export default function SideBar() {
   return (
-    <div className="fixed top-20 left-0 h-screen w-16 md:w-18m-0flex lg:w-20 flex-col bg-primary-bg text-white shadow-l">
-      <div className="mt-4">
-        <RocketIcon
-          className="relative flex items-center justify-center 
+    <div className="fixed top-20 left-0 h-screen w-16 md:w-18m-0flex lg:w-20 flex-col bg-oxford-blue text-white shadow-l">
+      <div
+        className="relative flex items-center justify-center 
         h-12 w-12 md:w-14 md:h-14 lg:w-16 lg:h-16 mt-2 mb-2 mx-auto shadow-lg p-2
-        bg-gray-700 text-blue-purple text-lg
-        hover:bg-blue-purple hover:text-white
+        bg-sonic-silver text-ultramarine-blue text-lg
+        hover:bg-cerulean-blue hover:text-snow
         rounded-3xl hover:rounded-xl
         transition-all duration-300 ease-linear
-        cursor-pointer"
-        />
+        cursor-pointer group"
+      >
+        <RocketIcon className="w-10 h-12" />
+
+        <span
+          className="absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md
+          text-snow bg-black-choral
+          text-xs font-bold 
+          transition-all duration-125 scale-0 origin-left group-hover:scale-125"
+        >
+          Start Quiz
+        </span>
       </div>
-      <div className="mt-4">
-        <LayersIcon
-          className="relative flex items-center justify-center 
-          h-12 w-12 md:w-14 md:h-14 lg:w-16 lg:h-16 mt-2 mb-2 mx-auto shadow-lg p-2
-          bg-gray-700 text-blue-purple text-lg
-          hover:bg-blue-purple hover:text-white
-          rounded-3xl hover:rounded-xl
-          transition-all duration-300 ease-linear
-          cursor-pointer"
-        />
+      <div
+        className="relative flex items-center justify-center 
+        h-12 w-12 md:w-14 md:h-14 lg:w-16 lg:h-16 mt-2 mb-2 mx-auto shadow-lg p-2
+        bg-sonic-silver text-ultramarine-blue text-lg
+        hover:bg-cerulean-blue hover:text-snow
+        rounded-3xl hover:rounded-xl
+        transition-all duration-300 ease-linear
+        cursor-pointer group"
+      >
+        <LayersIcon className="w-10 h-12" />
+        <span
+          className="absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md
+          text-snow bg-black-choral
+          text-xs font-bold 
+          transition-all duration-125 scale-0 origin-left group-hover:scale-125"
+        >
+          Path
+        </span>
       </div>
-      <div className="mt-4">
-        <ArchiveIcon
-          className="relative flex items-center justify-center 
+      <div
+        className="relative flex items-center justify-center 
          h-12 w-12 md:w-14 md:h-14 lg:w-16 lg:h-16 mt-2 mb-2 mx-auto shadow-lg p-2
-         bg-gray-700 text-blue-purple text-lg
-         hover:bg-blue-purple hover:text-white
+         bg-sonic-silver text-ultramarine-blue text-lg
+         hover:bg-cerulean-blue hover:text-snow
          rounded-3xl hover:rounded-xl
          transition-all duration-300 ease-linear
-         cursor-pointer"
-        />
+         cursor-pointer group"
+      >
+        <ArchiveIcon className="w-10 h-12" />
+        <span
+          className="absolute w-auto p-2 m-2 min-w-max left-14 rounded-md shadow-md
+          text-snow bg-black-choral
+          text-xs font-bold 
+          transition-all duration-125 scale-0 origin-left group-hover:scale-125"
+        >
+          History
+        </span>
       </div>
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,10 +11,24 @@ module.exports = {
 
     extend: {
       colors: {
-        "primary-bg": "#1f293b",
-        "my-blue": "#475569",
-        "electric": "#29DEF2",
-        "blue-purple": "#5068F2"
+        // current best-combo
+        "oxford-blue": "#1f293b",
+        "sky-blue": "#29DEF2",
+        "ultramarine-blue": "#5068F2",
+        // current best-combo
+        "black-choral": "#475569",
+        "spanish-gray": "#BABABA",
+        "sonic-silver":
+          "#6F86A5",
+        "snow":
+          "#FFFBFC",
+        "cerulean-blue":
+          "#4458C7",
+        //test
+        "blue-green": "#6CB0A8",
+        "persian-indigo": "#290773",
+        "prussian-blue": "#333F54",
+        "ultramarine-blue-2": "#677CF4"
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
