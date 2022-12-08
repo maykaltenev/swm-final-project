df225e40e33862deb42ab5a0704462f83edba539
sgujuluva <sanghee04@gmail.com>
2022-12-08 10:36:48 +0100

    nothing changed

--- client/src/components/MyDocument/MyDocument.jsx
+++ client/src/components/MyDocument/MyDocument.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
 import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
-import {Certificate} from "../Certificate/Certificate"
+import { Certificate } from "../Certificate/Certificate";
 import { UserContext } from "../Context/UserContext";
 //images
 import reactimg from "../../assets/reactimg.png";
@@ -20,16 +20,16 @@ const styles = StyleSheet.create({
     padding: 10,
     flexGrow: 1,
   },
-  document:{
+  document: {
     /* maxHeight: "1200", */
-    overflowY: "auto" 
-  }
+    overflowY: "auto",
+  },
 });
 
 // Create Document Component
 const MyDocument = () => {
-  const{user}= useContext(UserContext);
-
+  const { user } = useContext(UserContext);
+  /* getting the quiz history array from the local storage */
   const getQuizHistoryFromLocalStorage = () => {
     const quizHistory = localStorage.getItem("user");
     if (quizHistory) {
@@ -38,74 +38,85 @@ const MyDocument = () => {
       return [];
     }
   };
+  /* assign the array from local storage to the state */
   const [quizHistory, setQuizHistory] = useState(
     getQuizHistoryFromLocalStorage
   );
-  return(
-  <>
-  <h5>My Certificates</h5>
-  {quizHistory.quizResults &&
-            quizHistory.quizResults.map((quiz) => (
-              quiz.resultPercentage ? 
-              <>
-               <PDFViewer>
+  return (
+    <>
+      <h5>My Certificates</h5>
+      {quizHistory.quizResults &&
+        quizHistory.quizResults.map((quiz) =>
+          quiz.resultPercentage ? (
+            <>
+              <PDFViewer>
                 <Certificate
-                date={quiz.createdOn} 
-                inputType={quiz.quizType} 
-                sessionId={quiz.sessionId}
-                 name={`${user.firstName} ${user.lastName } `} 
-                 percentage={quiz.resultPercentage}
-                 inputTypeImage={quiz?.quizType === "javascript"
-                ? jsimg
-                : quiz?.quizType === "react"
-                ? reactimg
-                : quiz?.quizType === "express"
-                ? expressimg
-                : quiz?.quizType === "mongodb"
-                ? mongodbimg
-                : quiz?.quizType === "nodejs"
-                ? nodeimg
-                : ""}
+                  date={quiz.createdOn}
+                  inputType={quiz.quizType}
+                  sessionId={quiz.sessionId}
+                  name={`${user.firstName} ${user.lastName} `}
+                  percentage={quiz.resultPercentage}
+                  inputTypeImage={
+                    quiz?.quizType === "javascript"
+                      ? jsimg
+                      : quiz?.quizType === "react"
+                      ? reactimg
+                      : quiz?.quizType === "express"
+                      ? expressimg
+                      : quiz?.quizType === "mongodb"
+                      ? mongodbimg
+                      : quiz?.quizType === "nodejs"
+                      ? nodeimg
+                      : ""
+                  }
                 />
-                 </PDFViewer>
-              <PDFDownloadLink document={<Certificate
-                date={quiz.createdOn} 
-                inputType={quiz.quizType.charAt(0).toUpperCase() + quiz.quizType.slice(1)} 
-             inputTypeImage={quiz?.quizType === "javascript"
-             ? jsimg
-             : quiz?.quizType === "react"
-             ? reactimg
-             : quiz?.quizType === "express"
-             ? expressimg
-             : quiz?.quizType === "mongodb"
-             ? mongodbimg
-             : quiz?.quizType === "nodejs"
-             ? nodeimg
-             : ""}
-                sessionId={quiz.sessionId}
-                 name={`${user.firstName} ${user.lastName} `} 
-                 percentage={quiz.resultPercentage}/> } fileName="certificate.pdf">
-          {({ blob, url, loading, error }) =>
-            loading ? "Loading document..." : <button className="pointer-cursor bg-btn-majorelle-blue border-2 border-bg-menu-dark-silver-metallic py-2 px-6 rounded hover:bg-link-violet-blue">Download now!</button>
-          }
-        </PDFDownloadLink> </>
-             :
-             ""
-           
-            ))
-            }
-           
-  {/*  <Document style={styles.document}>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-     
-    </Page>
-  </Document>  */}
-  
- </>
-  )
+              </PDFViewer>
+              <PDFDownloadLink
+                document={
+                  <Certificate
+                    date={quiz.createdOn}
+                    inputType={
+                      quiz.quizType.charAt(0).toUpperCase() +
+                      quiz.quizType.slice(1)
+                    }
+                    inputTypeImage={
+                      quiz?.quizType === "javascript"
+                        ? jsimg
+                        : quiz?.quizType === "react"
+                        ? reactimg
+                        : quiz?.quizType === "express"
+                        ? expressimg
+                        : quiz?.quizType === "mongodb"
+                        ? mongodbimg
+                        : quiz?.quizType === "nodejs"
+                        ? nodeimg
+                        : ""
+                    }
+                    sessionId={quiz.sessionId}
+                    name={`${user.firstName} ${user.lastName} `}
+                    percentage={quiz.resultPercentage}
+                  />
+                }
+                fileName="certificate.pdf"
+              >
+                {({ blob, url, loading, error }) =>
+                  loading ? (
+                    "Loading document..."
+                  ) : (
+                    <button className="pointer-cursor bg-btn-majorelle-blue border-2 border-bg-menu-dark-silver-metallic py-2 px-6 rounded hover:bg-link-violet-blue">
+                      Download now!
+                    </button>
+                  )
+                }
+              </PDFDownloadLink>{" "}
+            </>
+          ) : (
+            ""
+          )
+        )}
+
+      </>
+  );
 };
 
 export default MyDocument;
