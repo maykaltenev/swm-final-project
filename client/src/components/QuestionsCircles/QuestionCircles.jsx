9e3851f3528ad68018201eb32f97e2784c5000db
Maykal Tenev <michaeltenev@gmail.com>
2022-12-08 16:19:25 +0100

    test

--- client/src/components/QuestionsCircles/QuestionCircles.jsx
+++ client/src/components/QuestionsCircles/QuestionCircles.jsx
@@ -13,7 +13,7 @@ export default function QuestionCircles({ setCurrentQuestion }) {
     <div className="flex items-center grid grid-cols-6 sm:grid-cols-3 sm:g-1 sm:h-[25%] border-2 dark:text-text-ghost-white sm:w-3/4 sm:h-1/2 sm:align-center">
       {questionData.map((question, i) => (
         <a
-          className="pointer w-4 h-4 border-4 rounded-full p-3 flex justify-center items-center "
+          className="pointer w-4 h-4 border-2 rounded-full p-3 flex justify-center items-center "
           style={{
             backgroundColor: marked.includes(question?._id) ? "lightgray" : "",
           }}
