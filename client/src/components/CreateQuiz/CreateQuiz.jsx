fb69390b5326ed97bfbcf3bc2c395b91bbde5e84
sgujuluva <sanghee04@gmail.com>
2022-12-09 13:36:36 +0100

    select dropdown

--- client/src/components/CreateQuiz/CreateQuiz.jsx
+++ client/src/components/CreateQuiz/CreateQuiz.jsx
@@ -1,11 +1,17 @@
 import React, { useContext, useEffect } from "react";
 import { useState } from "react";
-
+import CheckboxDropdownComponent, {
+  createStyles
+} from "react-checkbox-dropdown";
 import { QuestionContext } from "../Context/QuestionContext";
 import { UserContext } from "../Context/UserContext";
 import SideBar from "../SideBar/SideBar";
 
+const options = ["React Beginner","React Intermediate","React Advanced","Javascript Beginner","Javascript Intermediate","Javascript Advanced",].map(item => ({value:item, label:item}))
+
 function CreateQuiz() {
+  const [checkboxValue, setCheckboxValue] = useState([]);
+
   const { timer } = useContext(UserContext);
   const [selectedQuestionType, setSelectedQuestionType] = useState("");
   const { handleCreateNewSession, setMarked, setSessionId, setQuestionData } =
@@ -31,8 +37,30 @@ function CreateQuiz() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  console.log("the value of selected option in create quiz is", checkboxValue)
   return (
-    <div>
+    <div className = "flex justify-around">
+      <div className="font-poppins">
+      <CheckboxDropdownComponent
+          displayText="Select your own Stack"
+          options={options}
+          onChange={option => {
+            if (!checkboxValue.includes(option)) {
+              const newValue = [...checkboxValue, option];
+             setCheckboxValue(newValue);
+            }
+          }}
+          onDeselectOption={option => {
+            const filteredOptions = checkboxValue.filter(
+              item => item.value !== option.value
+            );
+           setCheckboxValue(filteredOptions);
+          }}
+          value={checkboxValue}
+         /*  displayTags */
+         /*  isStrict={false} */
+        />
+      </div>
       <select onChange={handleChangeOption}>
         <option value={""}>Select</option>
         <option value="react">React</option>
