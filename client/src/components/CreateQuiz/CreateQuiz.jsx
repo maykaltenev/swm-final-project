f8b8333c5d904cbe4098f98b5c6c5e6138bd3456
sgujuluva <sanghee04@gmail.com>
2022-12-09 15:16:43 +0100

    selcted multi group options from library

--- client/src/components/CreateQuiz/CreateQuiz.jsx
+++ client/src/components/CreateQuiz/CreateQuiz.jsx
@@ -1,13 +1,21 @@
 import React, { useContext, useEffect } from "react";
 import { useState } from "react";
 import CheckboxDropdownComponent, {
-  createStyles
+  createStyles,
 } from "react-checkbox-dropdown";
+import Multiselect from "multiselect-react-dropdown";
 import { QuestionContext } from "../Context/QuestionContext";
 import { UserContext } from "../Context/UserContext";
 import SideBar from "../SideBar/SideBar";
 
-const options = ["React Beginner","React Intermediate","React Advanced","Javascript Beginner","Javascript Intermediate","Javascript Advanced",].map(item => ({value:item, label:item}))
+const options = [
+  "React Beginner",
+  "React Intermediate",
+  "React Advanced",
+  "Javascript Beginner",
+  "Javascript Intermediate",
+  "Javascript Advanced",
+].map((item) => ({ value: item, label: item }));
 
 function CreateQuiz() {
   const [checkboxValue, setCheckboxValue] = useState([]);
@@ -37,74 +45,73 @@ function CreateQuiz() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  console.log("the value of selected option in create quiz is", checkboxValue)
+  console.log("the value of selected option in create quiz is", checkboxValue);
+
+
+
   return (
-    <div className = "flex justify-around">
-     <div className="font-poppins">
-      <CheckboxDropdownComponent
-          displayText="Select your own Stack"
-          options={options}
-          onChange={option => {
-            if (!checkboxValue.includes(option)) {
-              const newValue = [...checkboxValue, option];
-             setCheckboxValue(newValue);
-            }
-          }}
-          onDeselectOption={option => {
-            const filteredOptions = checkboxValue.filter(
-              item => item.value !== option.value
-            );
-           setCheckboxValue(filteredOptions);
-          }}
-          value={checkboxValue}
-          />
-      </div> 
-          {/*  displayTags 
-           isStrict={false} */} 
-      <select onChange={handleChangeOption}>
-        <option value={""}>Select</option>
-        <optgroup label="React">
-        <option value="react"><input type="checkbox" />React Beginner</option>
-        </optgroup>
-        <option value="nodejs">Nodejs</option>
-        <option value="javascript">JavaScript</option>
-        <option value="express">Express</option>
-        <option value="mongodb">MongoDB</option>
-      </select>
-      <select onChange={handleChangeOption}>
-  <option value={""}>Select</option>
-  <optgroup label="React">
-    <option value="react"><input type="checkbox" />React Beginner</option>
-  </optgroup>
-  <option value="nodejs">Nodejs</option>
-  <option value="javascript">JavaScript</option>
-  <option value="express">Express</option>
-  <option value="mongodb">MongoDB</option>
-</select>
-      <select>
-      <optiongroup label="Fruits">
-        <option>
-          <input type="checkbox" /> Apple
-        </option>
-        <option>
-          <input type="checkbox" /> Orange
-        </option>
-        <option>
-          <input type="checkbox" /> Banana
-        </option>
-      </optiongroup>
-      <optiongroup label="Vegetables">
-        <option>
-          <input type="checkbox" /> Carrot
-        </option>
-        <option>
-          <input type="checkbox" /> Broccoli
-        </option>
-        <option>
-          <input type="checkbox" /> Spinach
-        </option>
-      </optiongroup>
-    </select>
+    <div className="flex justify-around">
+      <Multiselect
+      placeholder="Select only 3"
+        selectionLimit="3"
+        displayValue="value"
+        groupBy="label"
+        onKeyPressFn={function noRefCheck() {}}
+        onRemove={function noRefCheck() {}}
+        onSearch={function noRefCheck() {}}
+        onSelect={function noRefCheck() {}}
+        options={[
+          {
+            label: "React",
+            value: "React Beginner",
+          },
+          {
+            label: "React",
+            value: "React Intermediate",
+          },
+          {
+            label: "React",
+            value: "React Advanced",
+          },
+          {
+            label: "Nodejs",
+            value: "Nodejs Beginner",
+          },
+          {
+            label: "Nodejs",
+            value: "Nodejs Intermediate",
+          },
+          {
+            label: "Nodejs",
+            value: "Nodejs Advanced",
+          },
+          {
+            label: "Expressjs",
+            value: "Expressjs beginner",
+          },
+          {
+            label: "Expressjs",
+            value: "Expressjs Intermediate",
+          },
+          {
+            label: "Expressjs",
+            value: "Expressjs Advanced",
+          },
+          {
+            label: "MongoDB",
+            value: "MongoDB beginner",
+          },
+          {
+            label: "MongoDB",
+            value: "MongoDB Intermediate",
+          },
+          {
+            label: "MongoDB",
+            value: "MongoDB Advanced",
+          },
+        ]}
+        showCheckbox        
+      />
       <button
         disabled={selectedQuestionType === ""}
         className="bg-ultramarine-blue-2 rounded w-50 px-4 text-3xl h-14 text-bold text-snow hover:bg-red"
