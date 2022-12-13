8d3b70b124bda616123de4563b3655a609bd84a7
sgujuluva <sanghee04@gmail.com>
2022-12-13 16:09:10 +0100

    footer border colors adjusted

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,109 +2,108 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer class="text-gray-600 body-font">
-      <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+    <footer className="mt-5 dark:bg-nav-raisin-black-2 text-gray-600 body-font shadow-4xl rounded-md ">
+      <div className=" container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           
-        <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
-          <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+        <div className=" flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+          <div className=" lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="dark:text-ultramarine-blue-2 title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
               Javascript Tutorials
             </h2>
-            <nav class="list-none mb-10">
+            <nav className="list-none mb-10">
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Beginner</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Beginner</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Intermediate</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Intermediate</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Advanced</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Advanced</a>
               </li>
             </nav>
           </div>
-          <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="dark:text-ultramarine-blue-2 title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
               React Tutorials
             </h2>
-            <nav class="list-none mb-10">
+            <nav className="dark:text-snow list-none mb-10">
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Beginner</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Beginner</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Intermediate</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Intermediate</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Advanced</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Advanced</a>
               </li>
             </nav>
           </div>
-          <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="dark:text-ultramarine-blue-2 title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
               Nodejs Tutorials
             </h2>
-            <nav class="list-none mb-10">
+            <nav className="dark:text-snow list-none mb-10">
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Beginner</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Beginner</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Intermediate</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Intermediate</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Advanced</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Advanced</a>
               </li>
             </nav>
           </div>
           
-          <div class="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
+          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="dark:text-ultramarine-blue-2 title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
               MongoDB Tutorials
             </h2>
-            <nav class="list-none mb-10">
+            <nav className="dark:text-snow list-none mb-10">
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Beginner</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Beginner</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Intermediate</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Intermediate</a>
               </li>
               <li>
-                <a class="text-gray-600 hover:text-gray-800">Advanced</a>
+                <a className="dark:text-snow text-gray-600 hover:text-gray-800">Advanced</a>
               </li>
-            
             </nav>
           </div>
         </div>
       </div>
-      <div class="bg-gray-100">
-        <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-          <p class="text-gray-500 text-sm text-center sm:text-left">
-            © 2020 MERN + Quiz app — Made with ❤️ by SWM
+      <div className="bg-gray-100 dark:bg-oxford-blue">
+        <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <p className="dark:text-ultramarine-blue text-gray-500 text-sm text-center sm:text-left">
+            © 2022 MERN +  Made with ❤️ by SWM
           </p>
-          <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-            <a class="text-gray-500">
+          <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+            <a className="dark:text-ultramarine-blue text-gray-500">
               <svg
                 fill="currentColor"
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                class="w-5 h-5"
+                className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
                 <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
               </svg>
             </a>
-            <a class="ml-3 text-gray-500">
+            <a className="dark:text-ultramarine-blue ml-3 text-gray-500">
               <svg
                 fill="currentColor"
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                class="w-5 h-5"
+                className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
                 <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
               </svg>
             </a>
-            <a class="ml-3 text-gray-500">
+            <a class="dark:text-ultramarine-blue ml-3 text-gray-500">
               <svg
                 fill="none"
                 stroke="currentColor"
@@ -118,7 +117,7 @@ function Footer() {
                 <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
               </svg>
             </a>
-            <a class="ml-3 text-gray-500">
+            <a class="dark:text-ultramarine-blue ml-3 text-gray-500">
               <svg
                 fill="currentColor"
                 stroke="currentColor"
